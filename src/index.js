9ab97229112955009b347e13dc178a48848802a4
huangzihao <346445684@qq.com>
2020-08-21 10:48:07 +0800

    obj

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Index from './pages/index';
-import Threejs from './pages/threejs';
-import Line from './pages/line';
 import Basis from './pages/basis';
+import Pikaqiu from './pages/pikaqiu';
+import Line from './pages/line';
 
 ReactDOM.render(
     <>
         <Router>
             <Route exact path="/" component={Index} />
-            <Route exact path="/threejs" component={Threejs} />
             <Route exact path="/line" component={Line} />
             <Route exact path="/basis" component={Basis} />
+            <Route exact path="/pikaqiu" component={Pikaqiu} />
         </Router>
     </>,
     document.getElementById('root')
