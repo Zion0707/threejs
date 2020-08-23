7e1ee20a262e4c4e7359fd195bf295e34774cdda
Zion0707 <zion19920707@gmail.com>
2020-08-23 15:12:06 +0800

    test

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,6 +1,15 @@
 import React, { useEffect } from 'react';
 function Test() {
-    useEffect(() => {});
-    return <>test</>;
+    const init = () => {
+        // dom元素生成
+        const contentEl = document.getElementById('content');
+        const winWidth = window.innerWidth;
+        const winHeight = window.innerHeight;
+        contentEl.style.cssText = `width:${winWidth}px;height:${winHeight}px`;
+    };
+    useEffect(() => {
+        init();
+    });
+    return <div id="content"></div>;
 }
 export default Test;
