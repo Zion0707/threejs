f710ce25a02b583ebf329d9b488f14b524645c48
huangzihao <346445684@qq.com>
2020-08-24 18:35:48 +0800

    car

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -5,58 +5,93 @@ import './index.css';
 
 function Test() {
     const init = () => {
-        // dom元素生成
-        const contentEl = document.getElementById('content');
+        const el = document.getElementById('content');
         const winWidth = window.innerWidth;
         const winHeight = window.innerHeight;
-        contentEl.style.cssText = `width:${winWidth}px;height:${winHeight}px`;
+        el.style.cssText = `width:${winWidth}px;height:${winHeight}px`;
 
+        // 场景
         const scene = new THREE.Scene();
 
-        const geometry = new THREE.BoxGeometry(10, 10, 10);
-        const material = new THREE.MeshLambertMaterial({
-            color: '#0000ff',
+        const carGroup = new THREE.Group();
+        carGroup.name = '汽车组';
+        const carBodyGeometry = new THREE.BoxGeometry(20, 4, 30);
+        carBodyGeometry.name = '车身';
+        const carBodyMaterial = new THREE.MeshLambertMaterial({ color: '#fff', transparent: true });
+        carBodyMaterial.opacity = 0.5;
+        const carBodyMesh = new THREE.Mesh(carBodyGeometry, carBodyMaterial);
+
+        const carWheels = new THREE.CylinderGeometry(5, 5, 4, 32);
+        const carWheelsMaterial = new THREE.MeshLambertMaterial({
+            color: '#000',
+            transparent: true,
+            wireframe: true,
         });
-        const mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+        const carWheelsLeftBefore = new THREE.Mesh(carWheels, carWheelsMaterial);
+        carWheelsLeftBefore.name = '左前轮';
+        carWheelsLeftBefore.rotation.x = 33;
+        carWheelsLeftBefore.rotation.z = 33;
+        carWheelsLeftBefore.position.x = 10;
+        carWheelsLeftBefore.position.z = 8;
 
-        setTimeout(() => {
-            scene.remove(mesh);
-        }, 1000);
+        const carWheelsRightBefore = carWheelsLeftBefore.clone();
+        carWheelsRightBefore.name = '右前轮';
+        carWheelsRightBefore.position.x = -10;
+        carWheelsRightBefore.position.z = 8;
 
-        const light = new THREE.DirectionalLight('#fff', 1);
-        scene.add(light);
+        const carWheelsLeftAfter = carWheelsLeftBefore.clone();
+        carWheelsLeftAfter.name = '左后轮';
+        carWheelsLeftAfter.position.x = 10;
+        carWheelsLeftAfter.position.z = -8;
 
-        // --------------------------------------------------------------------------------
+        const carWheelsRightAfter = carWheelsLeftBefore.clone();
+        carWheelsRightAfter.name = '右后轮';
+        carWheelsRightAfter.position.x = -10;
+        carWheelsRightAfter.position.z = -8;
 
-        const camera = new THREE.PerspectiveCamera(130, winWidth / winHeight, 0.1, 200);
-        camera.position.set(10, 30, 10);
-        camera.lookAt(scene.position); // 设置相机方向(指向的场景对象)
+        // 添加到组里面
+        carGroup.add(
+            carBodyMesh,
+            carWheelsLeftBefore,
+            carWheelsRightBefore,
+            carWheelsLeftAfter,
+            carWheelsRightAfter
+        );
+        scene.add(carGroup);
 
-        const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-        new OrbitControls(camera, renderer.domElement);
+        // 相机
+        const camera = new THREE.PerspectiveCamera(45, winWidth / winHeight, 0.1, 1000);
+        // 设置相机坐标
+        camera.position.set(150, 50, 50);
+        // 渲染器
+        const renderer = new THREE.WebGLRenderer();
 
         // 设置渲染器的颜色和大小
-        renderer.setClearColor('#f1f1f1');
+        renderer.setClearColor(0x404040);
         renderer.setSize(winWidth, winHeight);
-        renderer.shadowMapEnabled = true;
-        contentEl.append(renderer.domElement);
+        // 将renderer（渲染器）的dom元素（renderer.domElement）添加到我们的HTML文档中。
+        // 这就是渲染器用来显示场景给我们看的<canvas>元素
+        document.body.appendChild(renderer.domElement);
 
-        // let options = true;
-        // 执行实时刷新
+        // 鼠标控制旋转
+        new OrbitControls(camera, renderer.domElement);
+
+        // 设置光源
+        const light = new THREE.DirectionalLight('#fff', 0.5);
+        light.position.setScalar(100);
+        scene.add(light);
+        scene.add(new THREE.AmbientLight('#fff', 0.5));
+
+        // 添加灰色网格线
+        scene.add(new THREE.AxesHelper(5));
+
+        el.append(renderer.domElement);
+
         function render() {
-            // if (options) {
-            //     mlm.opacity += 0.01;
-            //     if (mlm.opacity >= 1) {
-            //         options = false;
-            //     }
-            // } else {
-            //     mlm.opacity -= 0.01;
-            //     if (mlm.opacity <= 0) {
-            //         options = true;
-            //     }
-            // }
+            carWheelsLeftBefore.rotation.x += 2;
+            carWheelsLeftAfter.rotation.x += 2;
+            carWheelsRightBefore.rotation.x += 2;
+            carWheelsRightAfter.rotation.x += 2;
 
             // 动画循环渲染
             requestAnimationFrame(render);
