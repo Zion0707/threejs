a9723d6289cc3bd93f4f409e80384cbf2380f8b2
huangzihao <346445684@qq.com>
2021-02-26 16:36:19 +0800

    change

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ import Guoxin from './pages/guoxin';
 import MeshLine from './pages/mesh-line';
 import Floating from './pages/floating';
 import FadeInOut from './pages/fade-in-out';
-import objOpacity from './pages/obj-opacity';
+import ObjOpacity from './pages/obj-opacity';
+import Guidao from './pages/guidao';
 
 ReactDOM.render(
     <>
@@ -51,7 +52,8 @@ ReactDOM.render(
                 <Route path="/mesh-line" component={MeshLine} />
                 <Route path="/floating" component={Floating} />
                 <Route path="/fade-in-out" component={FadeInOut} />
-                <Route path="/obj-opacity" component={objOpacity} />
+                <Route path="/obj-opacity" component={ObjOpacity} />
+                <Route path="/guidao" component={Guidao} />
             </Switch>
         </Router>
     </>,
